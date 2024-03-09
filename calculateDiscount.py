7b4a6dc083e3bc4c53ae133efaf56e800b246606
Bruno Oyuko <84352218+oyuko-bruno@users.noreply.github.com>
2024-03-09 12:51:38 +0300

    Add files via upload

---
+++ calculateDiscount.py
@@ -0,0 +1,15 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discounted_price = price-(price * discount_percent / 100)
+        return discounted_price
+    else:
+        return price
+
+# Prompt user for input60
+    
+original_price = float(input("Enter the original price of the item: "))
+discount_percentage = float(input("Enter the discount percentage: "))
+
+# Calculate and print the final price
+final_price = calculate_discount(original_price, discount_percentage)
+print(f"The final price after applying the discount is: ${final_price:.2f}")
